27c4204240018e02c610e6213c66de0bfe5e35dc
Maryna Berezhanska <arina.lex2012@gmail.com>
2021-05-15 17:09:10 +0300

    main markup

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import './sass/main.scss';
+
+const timer = {
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2019'),
+
+    start() {
+        const startTime = Date.now();
+
+        setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = currentTime - startTime;
+            const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+            console.log(`${days} : ${hours} : ${mins} : ${secs}`);
+        }, 1000)
+    },
+};
+
+timer.start();
+
+// new CountdownTimer({
+//     selector: '#timer-1',
+//     targetDate: new Date('Jul 17, 2019'),
+//   });
+
+function pad(value){
+    return String(value).padStart(2, '0');
+};
+
+function getTimeComponents(time){
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    
+    return { days, hours, mins, secs};
+};
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+// const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+// 1 января 1970 00:00, миллисекунды
