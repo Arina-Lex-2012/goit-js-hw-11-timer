40c580d48278a544a6e461ca2c4c521dd838efb0
Maryna Berezhanska <arina.lex2012@gmail.com>
2021-05-15 19:16:55 +0300

    days, hours

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 import './sass/main.scss';
 
+const refs = {
+   clockface: document.getElementById('timer-1'),
+   a: document.body.querySelectorAll('[data-value]'),
+};
+
+const a = document.body.querySelectorAll('[data-value]');
+console.log(a.forEach(element => console.log(element)));
+console.log(refs.a.forEach(element => console.log(element)));
+
+function updateClockface({ days, hours, mins, secs }) {
+    refs.a = `${days} : ${hours} : ${mins} : ${secs}`;
+};
+
+updateClockface(Date.now());
+
 const timer = {
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
+    targetDate: new Date('Sep 01, 2021'),
 
     start() {
-        const startTime = Date.now();
+        // const startTime = Date.now();
 
         setInterval(() => {
             const currentTime = Date.now();
-            const deltaTime = currentTime - startTime;
-            const { days, hours, mins, secs } = getTimeComponents(deltaTime);
-            console.log(`${days} : ${hours} : ${mins} : ${secs}`);
+            const deltaTime = this.targetDate - currentTime;
+            const { days, hours, mins, secs } = getTimeComponents(deltaTime);            
+            // console.log(`${days} : ${hours} : ${mins} : ${secs}`);
         }, 1000)
     },
 };
 
 timer.start();
-
-// new CountdownTimer({
-//     selector: '#timer-1',
-//     targetDate: new Date('Jul 17, 2019'),
-//   });
 
 function pad(value){
     return String(value).padStart(2, '0');
@@ -35,6 +45,13 @@ function getTimeComponents(time){
     
     return { days, hours, mins, secs};
 };
+
+
+
+
+
+
+
 // const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
 // const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -44,3 +61,9 @@ function getTimeComponents(time){
 // const secs = Math.floor((time % (1000 * 60)) / 1000);
 
 // 1 января 1970 00:00, миллисекунды
+
+
+// new CountdownTimer({
+//     selector: '#timer-1',
+//     targetDate: new Date('Jul 17, 2019'),
+//   });
